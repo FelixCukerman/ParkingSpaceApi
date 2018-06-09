38ebd893916cf4ef5e38edaac32bc0995e5af3af
midian <midian.ip@gmail.com>
2018-06-09 21:47:00 +0300

    service added

--- ParkingApi/Startup.cs
+++ ParkingApi/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+ using ParkingApi.Services;
 
 namespace ParkingApi
 {
@@ -24,6 +25,7 @@ namespace ParkingApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddTransient<CarService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
